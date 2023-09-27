bd25061231112c37bebf495fb1237bf45b88c7f4
Matias Risco <matiasriscolagos@gmail.com>
2023-09-27 19:52:20 +0200

    Refactor code in index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-const userCardElement = document.querySelector(".user-card");
-const hiddenCardElement = document.querySelector(".hidden-card");
-
 const setCardInUserInterFace = (element, card) => {
   const cardSuitElements = element.querySelectorAll(".card-suit");
   cardSuitElements.forEach(
@@ -12,6 +9,9 @@ const setCardInUserInterFace = (element, card) => {
     (cardSuitElement) => (cardSuitElement.textContent = card.figure)
   );
 };
+
+const userCardElement = document.querySelector(".user-card");
+const hiddenCardElement = document.querySelector(".hidden-card");
 
 const deck = createDeckOfCards(cardType);
 
