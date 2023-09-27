420f8945de5d48ac84e1d4db467c0473508754e5
Matias Risco <matiasriscolagos@gmail.com>
2023-09-27 19:12:05 +0200

    Hot fix in figure

--- js/cards.js
+++ js/cards.js
@@ -15,20 +15,5 @@ const isUserCardBigger = (userCard, hiddenCard) =>
 
 const cardType = {
   suits: ["♣", "♦", "♥", "♠"],
-  figure: [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A",
-  ],
+  figure: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
 };
